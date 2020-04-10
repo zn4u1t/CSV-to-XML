5fdff5b5e2889adfea2513c1cb7a52c978f13250
zn4u1t <33137041+zn4u1t@users.noreply.github.com>
2020-04-10 16:51:47 -0400

    Add files via upload

---
+++ CsvtoXml.py
@@ -0,0 +1,40 @@
+# CSV to XML Converter
+# Main
+# Zach Nault
+# 3/24/2019
+# Converts CSV files to XML format.
+# Change Log:
+
+import csv
+import datetime
+
+csvFile = input("Enter file name to be converted: ")
+now = datetime.datetime.now()
+xmlFile = 'myData' + now.strftime("%m-%d") + '.xml'
+
+csvData = csv.reader(open(csvFile))
+xmlData = open(xmlFile, 'w')
+xmlData.write('<?xml version="1.0" encoding="utf-8" standalone="yes"?>' + "\n")
+# there must be only one top-level tag
+xmlData.write('<Room_List>' + "\n")
+
+rowNum = 0
+for row in csvData:
+    if rowNum == 0:
+        tags = row
+        for i in range(len(tags)):
+            tags[i] = tags[i].replace('   ', '')
+    else: 
+        xmlData.write('  <Class>' + "\n")
+        for i in range(len(tags)):
+            #replace ampersand with character entity.
+            row[i] = row[i].replace('&', '&amp;')
+            row[i] = row[i].replace('  ', '')
+            xmlData.write( '    <' + str(tags[i]) + '>' \
+                          + str(row[i]) + '</' + str(tags[i]) + '>' + "\n")
+        xmlData.write('  </Class>' + "\n")
+            
+    rowNum +=1
+
+xmlData.write('</Room_List>' + "\n")
+xmlData.close()
---
+++ changeLogGen.py
@@ -0,0 +1,23 @@
+# Change Log Generator
+# Main
+# Zach Nault
+# 3/26/2019
+# Creates a changelog between two same format CSV files. 
+# Change Log:
+
+import datetime
+
+oldDoc = input("Enter original file: ")
+newDoc = input("Enter new file: ")
+now = datetime.datetime.now()
+with open(oldDoc, 'r') as t1, open(newDoc, 'r') as t2:
+    fileone = t1.readlines()
+    filetwo = t2.readlines()
+with open('ChangeLog' + now.strftime("%m-%d") + '.csv', 'w') as out1, open('RoomListRept.csv', 'r') as l1:
+    line1 = l1.readline()
+    out1.write(line1)
+    for line in filetwo:
+        if line not in fileone:
+            out1.write(line)
+print("Look in file ChangeLog" + now.strftime("%m-%d") + ".csv for changes.")
+
